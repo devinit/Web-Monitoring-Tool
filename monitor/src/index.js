fcf09bb40a7a619f7a4b82fc701a168b4b41e615
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-10-31 14:18:52 +0300

    Add material-pro theme

--- monitor/src/index.js
+++ monitor/src/index.js
@@ -1,1 +1,9 @@
-const test = () => "Hello World";
+import React from 'react';
+import { render } from 'react-dom'
+
+const App = () => <div>React Live</div>;
+
+const appDom = document.getElementById('app');
+if (appDom) {
+    render(<App/>, appDom);
+}
