65d4fa45ca53561cea020b3d64dd2052c06f5579
David Ebukali <debukali@gmail.com>
2020-06-13 02:27:45 +0300

    Use devinitautomailer gmail account to send emails

--- web_monitor/settings.py
+++ web_monitor/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'debukali@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'xulplmxouehardzh')
+EMAIL_HOST_USER = 'devinitautomailer@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
